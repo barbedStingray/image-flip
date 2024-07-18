b67ab385779c7114ed8ba83d0f0b18813bc1c11a
Ben Rehmann <brehmann@me.com>
2024-07-18 16:14:48 -0500

    flippy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
+
   return (
     <div className="App">
-      <div className='card'>
-        <div className='aSide'>
-          <h1>A SIDE</h1>
-        </div>
-        <div className='bSide'>
-          <h1>B SIDE</h1>
+      <div className='card' onClick={handleClick}>
+        <div className={`card-inner ${flipped ? 'flipped' : ''}`}>
+          <div className='card-side aSide'>
+            <h1>A SIDE</h1>
+          </div>
+          <div className='card-side bSide'>
+            <h1>B SIDE</h1>
+          </div>
         </div>
       </div>
     </div>
